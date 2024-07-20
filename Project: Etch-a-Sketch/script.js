c0246c8455f005a43c7007b5c97af952f0974bf5
nastike <subheshjoshi@yahoo.com>
2024-07-20 17:27:50 +0545

    added elements from Javascript and changed the hover property

--- Project: Etch-a-Sketch/script.js
+++ Project: Etch-a-Sketch/script.js
@@ -1,1 +1,19 @@
-console.log("from console")
+const container = document.querySelector("#container")
+const body = document.querySelector("body")
+
+const square = document.createElement("div")
+
+const arrayOfSquares = [];
+
+for (let i = 0; i < 16; i++) {
+    arrayOfSquares.push(document.createElement("div"))
+
+
+}
+
+arrayOfSquares.forEach(square => {
+    container.appendChild(square)
+    square.classList.add("squares")
+})
+
+
