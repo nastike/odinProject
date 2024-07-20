0ee5928a77ecd3d72c1c16410c121313fb86cdb7
nastike <subheshjoshi@yahoo.com>
2024-07-20 18:54:51 +0545

    added clumn

--- Project: Etch-a-Sketch/script.js
+++ Project: Etch-a-Sketch/script.js
@@ -1,19 +1,28 @@
 const container = document.querySelector("#container")
 const body = document.querySelector("body")
+const button = document.createElement("button")
 
 const square = document.createElement("div")
 
+const arrayOfColumns = [];
 const arrayOfSquares = [];
 
-for (let i = 0; i < 16; i++) {
-    arrayOfSquares.push(document.createElement("div"))
-
-
+for (let i = 0; i < 4; i++) {
+    arrayOfColumns.push(document.createElement("div"))
 }
 
-arrayOfSquares.forEach(square => {
-    container.appendChild(square)
-    square.classList.add("squares")
+arrayOfColumns.forEach((column)=>{
+    for (let i = 0; i < 4; i++) {
+        arrayOfSquares.push(document.createElement("div"))
+
+    }
 })
+arrayOfColumns.forEach(column => {
+    container.appendChild(column)
+    column.classList.add("column")
+})
+
+
+// body.appendChild(button)
 
 
