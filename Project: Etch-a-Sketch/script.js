9cd84ecd1877d577ba5e31bb63842118b587366a
nastike <subheshjoshi@yahoo.com>
2024-07-23 13:15:39 +0545

    Added reset button on the top, restting the page and reloading the grid

--- Project: Etch-a-Sketch/script.js
+++ Project: Etch-a-Sketch/script.js
@@ -1,23 +1,34 @@
 const container = document.querySelector("#container")
-
-let number = parseInt(prompt("What is the size of your grid? (for 100*100) Enter 100  "))
+const button = document.querySelector("button")
+let number;
 
 const arrayOfBoxes = []
-container.setAttribute("id", "container")
 
-if(number <100){
-    for (let i = 0; i < number*number; i++) {
-        const individualSquares = document.createElement("div");
-        arrayOfBoxes.push(individualSquares)
-        
-    }
-}else{
-    number = parseInt(prompt("The limit of the size is 100, please try again "))
-}
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+function getNumberOfGrid(){
+    number = parseInt(prompt("What is the size of your grid? (for 100*100) Enter 100  "))
+    
+    if(number <100){
+        for (let i = 0; i < number*number; i++) {
+            const individualSquares = document.createElement("div");
+            arrayOfBoxes.push(individualSquares)
+            
+        }
+    }else{
+        number = parseInt(prompt("The limit of the size is 100, please try again "))
+    }
+   return arrayOfBoxes;
+}
+
+
+getNumberOfGrid();
 //rgba(2, 154, 1, 0.6)
+
 function getRandomRGBAColor() {
     let r = getRandomInt(0, 255);
     let g = getRandomInt(0, 255);
@@ -27,9 +38,8 @@ function getRandomRGBAColor() {
 }
 
 
-
 arrayOfBoxes.forEach((element)=>{
-    let a = 0.5;
+    let a = 0.1;
     element.classList.add("squares");
     element.style.height = `${(800/number)}px`;
     element.style.width = `${(800/number)}px`;
@@ -43,3 +53,6 @@ arrayOfBoxes.forEach((element)=>{
     container.appendChild(element);
 })
 
+button.addEventListener("click",()=>{
+    window.location.reload()
+})
