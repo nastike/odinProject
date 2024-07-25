bbb388aae96f11f2626e7f646f7e86b4a8b6face
nastike <subheshjoshi@yahoo.com>
2024-07-25 14:08:53 +0545

    Added Functionality to AC and Delete Button

--- calculator-app/script.js
+++ calculator-app/script.js
@@ -5,16 +5,37 @@ let displayText = '';
 
 bts.forEach((element)=>{
     element.addEventListener("click", (e)=>{
-        if(display.textContent.length > 7){
-            displayText = displayText;
+        if(e.target.value === "1" || e.target.value === "2" ||e.target.value === "3" || e.target.value === "4"||
+            e.target.value === "5" || e.target.value === "6" ||e.target.value === "7" || e.target.value === "8"||
+            e.target.value === "9" || e.target.value === "0" ){
+                if(display.textContent.length > 7){
+                    displayText = displayText;
+                    
+                    
+                }else{
+                    
+                    displayText = e.target.value;
+                    display.textContent += displayText;
+                    
+                    
+                }
+            }
+      
+        if(e.target.value === "+" || e.target.value === "-" ||e.target.value === "*" || e.target.value === "/"){
+                
+
+
+        }
+        if(e.target.value ==="DEL"){
+            const someArr = display.textContent.split("")
+            someArr.pop()
+            displayText = someArr.join('')
+            display.textContent = displayText;
             
-            
-        }else{
-            
-            displayText = e.target.value;
-            display.textContent += displayText;
-            
-            
+        }
+        if(e.target.value ==="AC"){
+            displayText = ''
+            display.textContent = displayText;
         }
        
     })
@@ -35,3 +56,4 @@ const divide = ()=>{
 const operate = ()=>{
     
 }
+
