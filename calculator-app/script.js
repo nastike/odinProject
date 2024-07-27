aa93ad77467a3bf3fee9f35de53f3f9512b538fb
nastike <subheshjoshi@yahoo.com>
2024-07-27 11:22:33 +0545

    Added eventliteners to every key, added functionality of ac btn and numbers

--- calculator-app/script.js
+++ calculator-app/script.js
@@ -1,136 +1,57 @@
-const bts = document.querySelectorAll(".number")
 const display = document.querySelector("#display")
-let displayText = ''; 
-let operationFirstValue = '';
-let operationSecondValue ='';
-let currentOperator = ''
-let result;
-let isOperatorClicked = false;
-
-bts.forEach((element)=>{
-    element.addEventListener("click", (e)=>{
-       const value = e.target.value
-        if(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(value)){
-
-                
-                if(display.textContent.length > 8){
-                    displayText = displayText;
-                    
-                    
-                }else{
-                    if(isOperatorClicked){
-                        display.textContent = '';
-                        isOperatorClicked = false
-                    }
-                    
-                    displayText = value;
-                    display.textContent += displayText;
-                    
-                    
-                }
-            }
-            if (value === ".") {
-                
-                if (!display.textContent.includes(".")) {
-                    if (display.textContent === '' || isOperatorClicked) {
-                        display.textContent = '0.'; 
-                    } else {
-                        display.textContent += value; 
-                    }
-                    isOperatorClicked = false; 
-                }
-            }
-        if(["+", "-", "*", "/", "%"].includes(value)){
-            if(operationFirstValue === ''){
-                operationFirstValue = display.textContent;
-            } else if(operationSecondValue ===''){
-                operationSecondValue = display.textContent;
-                result = operate(value, +operationFirstValue, +operationSecondValue);
-                display.textContent = result;
-                operationFirstValue = result;
-                operationSecondValue = '';
-            }
-            currentOperator = value;
-            isOperatorClicked = true;
-                
-                
-                
-
-                
-
-
-        }
-        if(value ==='eql'){
-            if(operationFirstValue !== '' && currentOperator !==''){
-                operationSecondValue = display.textContent;
-                result = operate(currentOperator, +operationFirstValue, +operationSecondValue)
-                display.textContent = result
-                operationFirstValue = ''
-                operationSecondValue = ''
-                currentOperator = ''
-            }
-        }
-        if(value ==="DEL"){
-            const someArr = display.textContent.split("")
-            someArr.pop()
-            displayText = someArr.join('')
-            display.textContent = displayText;
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+const equalBtn = document.querySelector(".equalsTo")
+const acBtn = document.querySelector(".acBtn")
+const delBtn = document.querySelector(".delBtn")
+let currentOperator = "";
+let firstOperand = "";
+let secondOperand = "";
+let displayText = '';
+let value
+numbers.forEach(number=>{
+    number.addEventListener("click", (e)=>{
+        value = e.target.value;
+        if(display.textContent.length > 8){
             
+            return 0
+
+        }else{
+            displayText = value;
+            display.textContent +=displayText 
         }
-        if(value ==="AC"){
-            displayText = ''
-            display.textContent = displayText;
-            operationFirstValue =''
-            operationSecondValue = ''
-            currentOperator = ''
+        
+    })
+
+})
+
+operators.forEach(operator=>{
+    operator.addEventListener("click", (e)=>{
+        value = e.target.value;
+        currentOperator = value;
+        if(firstOperand != ""){
+            firstOperand = display.textContent;
         }
-       
+        displayText += value;
+        display.textContent += displayText;
     })
 })
-const add = (a, b)=>{
-    return a+b;
-}
 
-const substract = (a, b)=>{
-    return a-b;
-}
-const multiply = (a, b)=>{
-    return a*b;
-}
-const divide = (a, b)=>{
-    return a/b;
-}
-const percentage = (num)=>{
-    console.log(num/100)
-    return (num/100);
+acBtn.addEventListener("click", (e)=>{
+    displayText = "";
+    currentOperator = "";
+    firstOperand = "";
+    secondOperand = "";
+    display.textContent = ""
+})
+delBtn.addEventListener("click", ()=>{
+    let someArr = display.textContent.split("")
+    someArr.pop()
+    display.textContent = someArr.join("")
+    
+    
+})
 
-}
-const operate = (str, firstValue, secondValue)=>{
-    switch(str){
-        case "+": 
-          return add(firstValue, secondValue)
-            
-        case "-":
-            return(substract(firstValue, secondValue))
-            console.log("from -")
-            
-        case "*":
-           
-            console.log("from *")
-            return(multiply(firstValue, secondValue))
-            
-        case "/":
-           
-            console.log("from /")
-            return(divide(firstValue, secondValue))
-            
-            
-        case "%":
-            console.log("from Percentage")
-            
-            return(percentage(firstValue))
-        default:
-            return 0    
-    }
-}
-
+equalBtn.addEventListener("click", (e)=>{
+    
+})
